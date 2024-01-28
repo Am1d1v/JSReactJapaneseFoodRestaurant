83379805be301c1506d3ba40fc2178fe49c0be24
Am1d1v <dimadiman1adima@gmail.com>
2024-01-28 13:42:59 +0300

    Seafood Restaurant Meals Section

---
+++ src/components/Meals/PromoText.jsx
@@ -0,0 +1,12 @@
+import styles from './PromoText.module.css';
+
+function PromoText() {
+  return (
+    <section className={styles['promo-text']}>
+      <h2>Seafood Restaurant</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, qui!</p>
+    </section>
+  )
+}
+
+export default PromoText
